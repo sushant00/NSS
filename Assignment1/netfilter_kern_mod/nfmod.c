8a10e60175de2121c66fdaccccde705c5d2b6269
sushant00 <sushant2964@gmail.com>
2019-02-18 02:41:54 +0530

    working nfmodule

--- Assignment1/netfilter_kern_mod/nfmod.c
+++ Assignment1/netfilter_kern_mod/nfmod.c
@@ -13,7 +13,7 @@ MODULE_DESCRIPTION("Netfilter module to identify and log type of network reconna
 
 static int nfmod_init(void);
 static void nfmod_exit(void);
-unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
+unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *));
 
 static struct nf_hook_ops nfho;
 
@@ -21,13 +21,13 @@ static int nfmod_init(void) {
     printk(KERN_INFO "nfmod: initialising netfilter kernel module...\n");
 
     nfho.hook = hook_func;
-    nfho.hooknum = NF_IP_PRE_ROUTING;
+    nfho.hooknum = NF_INET_PRE_ROUTING;
     nfho.pf = PF_INET;
     nfho.priority = NF_IP_PRI_FIRST;
     
     nf_register_hook(&nfho);
 
-    printk(KERN_INFO "nfmod: initialised\n");
+    printk(KERN_INFO "nfmod: initialised\n\n");
     return 0;
 }
 
@@ -39,62 +39,74 @@ static void nfmod_exit(void) {
     printk(KERN_INFO "nfmod: uninstalled nfmod\n");
 }
 
-unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *)) {
+unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *)) {
     struct iphdr *ip_header;
     struct tcphdr *tcp_header;
 
     unsigned int Flags[6];
-    char FlagsStr[6];
-    ip_header = ip_hdr(skb);
+    char FlagsStr[7];
+//    printk(KERN_INFO "nfmod: hook func called\n");
 
-    printk(KERN_INFO "nfmod: hook func called\n");
+    ip_header = ip_hdr(skb);
 
     if(ip_header->protocol == IPPROTO_TCP) {
         printk(KERN_INFO "nfmod: TCP Packet captured\n");
-       
 	tcp_header = tcp_hdr(skb);
 
-       	printk(KERN_INFO "nfmod: TCP Source Port: %u\n", tcp_header->source);
-        printk(KERN_INFO "nfmod: TCP Destination Port: %u\n", tcp_header->dest);
+//       	printk(KERN_INFO "nfmod: TCP Source Port: %u\n", tcp_header->source);
+//        printk(KERN_INFO "nfmod: TCP Destination Port: %u\n", tcp_header->dest);
 
         //find the flags set in the packet
+        FlagsStr[6] = 0;
         if(tcp_header->urg){
             FlagsStr[0] = 'U';
+            Flags[0] = 1;
         }else {
             FlagsStr[0] = '-';
+            Flags[0] = 0;
         }
         
-        if(tcp_header->urg){
+        if(tcp_header->ack){
             FlagsStr[1] = 'A';
+            Flags[1] = 1;
         }else {
             FlagsStr[1] = '-';
+            Flags[1] = 0;
         }
         
-        if(tcp_header->urg){
+        if(tcp_header->psh){
             FlagsStr[2] = 'P';
+            Flags[2] = 1;
         }else {
             FlagsStr[2] = '-';
+            Flags[2] = 0;
         }
         
-        if(tcp_header->urg){
+        if(tcp_header->rst){
             FlagsStr[3] = 'R';
+            Flags[3] = 1;
         }else {
             FlagsStr[3] = '-';
+            Flags[3] = 0;
         }
         
-        if(tcp_header->urg){
+        if(tcp_header->syn){
             FlagsStr[4] = 'S';
+            Flags[4] = 1;
         }else {
             FlagsStr[4] = '-';
+            Flags[4] = 0;
         }
         
-        if(tcp_header->urg){
+        if(tcp_header->fin){
             FlagsStr[5] = 'F';
+            Flags[5] = 1;
         }else {
             FlagsStr[5] = '-';
+            Flags[5] = 0;
         }
 
-        printk(KERN_INFO "nfmod: TCP Packet Flags %s\n", FlagsStr);
+//        printk(KERN_INFO "nfmod: TCP Packet Flags %s\n", FlagsStr);
 
         if(!Flags[0] && !Flags[1] && !Flags[2] && !Flags[3] && !Flags[4] && !Flags[5] ) {
             printk(KERN_INFO "nfmod: TCP NULL SCAN\n");
@@ -113,7 +125,7 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct 
         }
     }
 
-    printk(KERN_INFO "nfmod: exiting hook func\n");
+//    printk(KERN_INFO "nfmod: exiting hook func\n\n");
     return NF_ACCEPT;
 }
 
