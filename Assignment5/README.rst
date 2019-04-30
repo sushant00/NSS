ad2d711e7828ff0f13fc1c03d34b86977dcdbf82
Sushant <sushant2964@gmail.com>
2019-04-30 20:51:43 +0530

    completed udp port scanning

--- Assignment5/README.rst
+++ Assignment5/README.rst
@@ -34,12 +34,19 @@ Please type in the ip of target machine in target_ip field.
 -S is for syn scan, -F is for fin scan.
 If port is set then only that port is checked
 
+For udp scanning the following command is used:
+
+.. code:: shell
+
+  sudo ./port_scanner_udp <target ip> [port]
+
 
 Design
 ======
 - This Scanner supports the SYN Scan and FIN Scan, which work similar to NMAP
 - SYN Scan: packets with SYN bit are send to all ports of target. In response if the SYN and the ACK bits are set then, it is assumed that the port is opened.
 - FIN Scan: packets with FIN bit are send to all ports of target. In response if the RST and the ACK bits are set then, it is assumed that the port is closed.
+- for udp port scanning it is checked that on sending a packet to port if ICMP type 3 code 3 message of host unreachable in received, in which case we mark the port as closed 
 
 
 Assumptions
@@ -53,6 +60,7 @@ Assumptions
 Commands Used
 =============
 -sudo nmap -sX 10.0.0.6
--sudo nc -lvnt 5555
+-sudo nc -lv 5555
+-sudo nc -luv 5555
 
 Developed by Sushant Kumar Singh
--- Assignment5/port_scanner_udp.c
+++ Assignment5/port_scanner_udp.c
@@ -1,8 +1,11 @@
 //Sushant Kumar Singh
 //2016103
 
-//reference:https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
-//https://www.quora.com/What-is-TCP-checksum
+//reference:https://www.binarytides.com/udp-syn-portscan-in-c-with-linux-sockets/
+//https://nmap.org/book/scan-methods-udp-scan.html
+//https://en.wikipedia.org/wiki/User_Datagram_Protocol#IPv4_Pseudo_Header
+//https://stackoverflow.com/questions/27164271/how-to-convert-sock-dgram-to-sock-raw
+//https://www.tenouk.com/Module43a.html
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +17,9 @@
 #include <arpa/inet.h>
 
 #include <netdb.h>
-#include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 
 #include <pthread.h>
 
@@ -34,38 +38,31 @@ struct pseudo_header{
 	unsigned int dst_addr;
 	unsigned char reserved;
 	unsigned char protocol;
-	unsigned short tcp_len;
+	unsigned short udp_len;
 
-	struct tcphdr tcp_h;
+	struct udphdr udp_h;
 };
 
-int syn_scan;
 struct sockaddr_in target_addr;
+
+int icmp_reply;
 
 // SERVER
 int main(int argc, char **args){
 	//dont buffer output
 	setbuf(stdout, NULL);
 
-	if(argc<3){
-		printf("please supply the type of scan and ip to scan\n");
+	if(argc<2){
+		printf("please supply ip to scan\n");
 		return -1;
 	}
-	if(strcmp(args[1], "-S")==0){
-		printf("doing syn scan\n");
-		syn_scan = 1;
-	}else{
-		printf("doing fin scan\n");
-		syn_scan = 0;
-	}
-
 	printf("starting server...\n");
 
 	int scannerSocket, len, ret, enable=1;
 	struct sockaddr_in local_addr, dst_addr;
 
 
-	scannerSocket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	scannerSocket = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
 	if(scannerSocket==-1){	//error
 		fprintf(stderr, "could not create server\n");
 		exit(1);
@@ -85,13 +82,13 @@ int main(int argc, char **args){
 	//IPv4 address
 	dst_addr.sin_family = AF_INET;
 	dst_addr.sin_port = 0;		//we would set this later
-	inet_pton(AF_INET, (const char *)args[2], &dst_addr.sin_addr);
+	inet_pton(AF_INET, (const char *)args[1], &dst_addr.sin_addr);
 
 	target_addr = dst_addr;
 	
 	printf("scanner ready\n");
 
-	//scan the tcp responses
+	//scan the udp responses
 	pthread_t scan_thread;
 	pthread_create(&scan_thread, NULL, scanResponse, (void *)&scannerSocket);
 
@@ -101,94 +98,79 @@ int main(int argc, char **args){
 	memset(datagram, 0, MSG_LEN);
 
 	struct iphdr *ip_h = (struct iphdr *) datagram;
-	struct tcphdr *tcp_h = (struct tcphdr *) (datagram + sizeof(struct iphdr));
+	struct udphdr *udp_h = (struct udphdr *) (datagram + sizeof(struct iphdr));
 	struct pseudo_header ps_h;
 
 	ip_h->ihl = 5; //ip header len
 	ip_h->version = 4;
-	ip_h->tos = 0;
-	ip_h->tot_len = sizeof(struct iphdr)+sizeof(struct tcphdr);
+	ip_h->tos = 16;
+	ip_h->tot_len = sizeof(struct iphdr)+sizeof(struct udphdr);
 	ip_h->id = htons(12345);
 	ip_h->frag_off = 0;
 	ip_h->ttl = 100;
-	ip_h->protocol = IPPROTO_TCP;
+	ip_h->protocol = IPPROTO_UDP;
 	ip_h->check = 0; //skip checksum field for now
 	ip_h->saddr = local_addr.sin_addr.s_addr;
 	ip_h->daddr = dst_addr.sin_addr.s_addr;
 
+	//add udphdr headers
+	udp_h->source = htons(SERVER_PORT);
+	udp_h->dest = 0; // we would set this later
+	udp_h->len = htons(sizeof(struct udphdr));
+	udp_h->check = 0;
+	
 	// now calc checksum
-	ip_h->check = calcCheckSum( (unsigned short *)datagram, (ip_h->tot_len)/2 );
-
-	//add tcp headers
-	tcp_h->source = htons(SERVER_PORT);
-	tcp_h->dest = 0; // we would set this later
-	tcp_h->seq = htonl(12345);
-	tcp_h->ack_seq = 0;
-	tcp_h->doff = sizeof(struct tcphdr)/4;
-	if(syn_scan){
-		tcp_h->fin = 0;
-	}else{
-		printf("setting fin\n");
-		tcp_h->fin = 1;
-	}
-	if(syn_scan){
-		printf("setting syn\n");
-		tcp_h->syn = 1;
-	}else{
-		tcp_h->syn = 0;
-	}
-	tcp_h->rst = 0;
-	tcp_h->psh = 0;
-	tcp_h->ack = 0;
-	tcp_h->urg = 0;
-	tcp_h->window = htons(MSG_LEN);
-	tcp_h->check = 0;
-	tcp_h->urg_ptr = 0;
+	ip_h->check = calcCheckSum( (unsigned short *)datagram, (ip_h->tot_len) );
 
 	printf("starting scan...\n");
 	
-	// while(1){
-		int port_min = PORT_MIN;
-		int port_max = PORT_MAX;
-		printf("checking port specs\n");
-		if(args[3]!=NULL){
-			port_min = atoi(args[3]);
-			port_max = atoi(args[3]);
-		}
-		printf("looping ports\n");
-		for(int port_num = port_min; port_num <= port_max; port_num++){
-			//set the port number
-			// printf("sending pkt to port %d\n", port_num);
-			tcp_h->dest = htons(port_num);
-			tcp_h->check = 0; //important as this is inside a loop, otherwise wrong checksum calculated
-			// printf("adding source addr to ps_h %d to %d\n", local_addr.sin_addr.s_addr, ps_h.src_addr);
-			ps_h.src_addr = local_addr.sin_addr.s_addr;
-			// printf("added source addr to ps_h\n");
-
-			// printf("adding dest addr to ps_h\n");
-			ps_h.dst_addr = dst_addr.sin_addr.s_addr;
-			// printf("added dest addr to ps_h\n");
-
-			ps_h.reserved = 0;
-			ps_h.protocol = IPPROTO_TCP;
-			// printf("adding tcplen to ps_h\n");
-			ps_h.tcp_len = htons( sizeof(struct tcphdr) );
-
-			// printf("calculating checksum\n");
-			memcpy(&ps_h.tcp_h, tcp_h, sizeof(struct tcphdr));
-			tcp_h->check = calcCheckSum( (unsigned short *)&ps_h, sizeof(struct pseudo_header));
-
-			size_t sendlen = sizeof(struct iphdr) + sizeof(struct tcphdr);
-			ret = sendto( scannerSocket, datagram, sendlen, 0, (struct sockaddr *)&dst_addr, sizeof(dst_addr));
-			if(ret < 0){
-				printf("error sending to port %d\n",port_num);
-				return -1;
-			}else{
-				printf("sent pkt to port %d\n", port_num);
-			}
-		// }
-
+	int port_min = PORT_MIN;
+	int port_max = PORT_MAX;
+	printf("checking port specs\n");
+	if(args[2]!=NULL){
+		port_min = atoi(args[2]);
+		port_max = atoi(args[2]);
 	}
+	printf("looping ports\n");
+	for(int port_num = port_min; port_num <= port_max; port_num++){
+		icmp_reply = 0;
+		//set the port number
+		// printf("sending pkt to port %d\n", port_num);
+		udp_h->dest = htons(port_num);
+		udp_h->check = 0; //important as this is inside a loop, otherwise wrong checksum calculated
+		// printf("adding source addr to ps_h %d to %d\n", local_addr.sin_addr.s_addr, ps_h.src_addr);
+		ps_h.src_addr = local_addr.sin_addr.s_addr;
+		// printf("added source addr to ps_h\n");
+
+		// printf("adding dest addr to ps_h\n");
+		ps_h.dst_addr = dst_addr.sin_addr.s_addr;
+		// printf("added dest addr to ps_h\n");
+
+		ps_h.reserved = 0;
+		ps_h.protocol = IPPROTO_UDP;
+		// printf("adding udplen to ps_h\n");
+		ps_h.udp_len = htons(sizeof(struct udphdr));
+
+		// printf("calculating checksum\n");
+		memcpy(&ps_h.udp_h, udp_h, sizeof(struct udphdr));
+		// udp_h->check = calcCheckSum( (unsigned short *)&ps_h, sizeof(struct pseudo_header));
+
+		size_t sendlen = sizeof(struct iphdr) + sizeof(struct udphdr);
+		ret = sendto( scannerSocket, datagram, sendlen, 0, (struct sockaddr *)&dst_addr, sizeof(dst_addr));
+		if(ret < 0){
+			printf("error sending to port %d\n",port_num);
+			return -1;
+		}else{
+			printf("sent pkt to port %d\n", port_num);
+		}
+		sleep(1);
+		if( icmp_reply == 1){
+			printf("scanResponse: Port %d/udp closed\n", port_num);
+		}else{
+			printf("scanResponse: Port %d/udp open | filered\n", port_num);
+		}
+	}
+
 	pthread_join(scan_thread, NULL);
 	return 0;
 }
@@ -201,7 +183,7 @@ void *scanResponse(void *args){
 	struct sockaddr from_addr;
 
 
-	recvSocket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	recvSocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if(recvSocket==-1){	//error
 		fprintf(stderr, "scanResponse: could not create receiver\n");
 		exit(1);
@@ -222,33 +204,31 @@ void *scanResponse(void *args){
 		}
 
 		struct iphdr *ip_h;
-		struct tcphdr *tcp_h;
+		struct icmphdr *icmp_h;
 		struct sockaddr_in src_addr, dst_addr;
 		memset(&src_addr, 0, sizeof(struct sockaddr_in));
 		memset(&dst_addr, 0, sizeof(struct sockaddr_in));
 
 		ip_h = (struct iphdr *)recv_buf;
 
-		if(ip_h->protocol == IPPROTO_TCP){
-			tcp_h = (struct tcphdr *)( recv_buf + (ip_h->ihl)*4 );
+		if(ip_h->protocol == IPPROTO_ICMP){
+			icmp_h = (struct icmphdr *)( recv_buf + (ip_h->ihl)*4 );
 			src_addr.sin_addr.s_addr = ip_h->saddr;
 			dst_addr.sin_addr.s_addr = ip_h->daddr;
 
 			//Syn and Ack are set means port is open
 			if( (src_addr.sin_addr.s_addr == target_addr.sin_addr.s_addr) ){
-				if ( syn_scan && (tcp_h->syn == 1) && (tcp_h->ack == 1) ) {
-					printf("scanResponse: Port %d/tcp open|filtered\n", ntohs(tcp_h->source));
-				}else if( !syn_scan && (tcp_h->rst == 1) && (tcp_h->ack == 1) ){
-					printf("scanResponse: Port %d/tcp closed|filtered\n", ntohs(tcp_h->source));
+				if ( (icmp_h->type == ICMP_DEST_UNREACH) && (icmp_h->code == ICMP_PORT_UNREACH) ) {
+					icmp_reply = 1;
 				}
 				// else{
-				// 	printf("scanResponse: Port %d syn %d, ack %d fin %d\n", ntohs(tcp_h->source), tcp_h->syn, tcp_h->ack, tcp_h->fin);
+				// 	printf("scanResponse: Port %d syn %d, ack %d fin %d\n", ntohs(udp_h->source), udp_h->syn, udp_h->ack, udp_h->fin);
 				// }
 			}else{
 				printf("scanResponse: target ip did not match\n");
 			}
 		}else{
-			printf("scanResponse: not a tcp protocol packet\n");
+			printf("scanResponse: not a icmp protocol packet\n");
 		}
 	}
 }
